635069b0bf6315ded51d0d141173fed3b4e35a11
Dominic Harshaw <harshawdominic@yahoo.com>
2021-01-31 22:49:58 -0500

    We may have overcomplicated things`

--- Scratch.java
+++ Scratch.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 class Scratch {
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
 
         try {
@@ -10,10 +10,10 @@ class Scratch {
 	    executeCommands();            
 
             //creating a fileWriter object that will write to a file given a file path
-            FileWriter fileWriter = new FileWriter("/home/58/n01382358/COP4620/Project1/Compilers_step1/Output.txt");
+            FileWriter fileWriter = new FileWriter(System.getProperty("user.dir")+"/Output.txt");
 
             Process p;
-            p = Runtime.getRuntime().exec("cat *.micro | grun Little prog -tokens");//cmd /c <SomeCommand>
+            p = Runtime.getRuntime().exec("grun Little prog " + System.getProperty("user.dir") + "/Step1/inputs/s* " + "-tokens");//cmd /c <SomeCommand>
             p.waitFor();// causes the current thread to wait until this process has ended
             BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String line;
@@ -21,13 +21,13 @@ class Scratch {
             while ((line = reader.readLine()) != null) {
                 fileWriter.write(line + "\n");
                 System.out.println(line);
-            }
+            }//while
             fileWriter.close();
 
-        }catch (Exception e){
+        }catch (Exception e){//try
             e.printStackTrace();
-        }
-    }
+        }//catch
+    }//main
 
 
         public static void executeCommands() throws IOException {
@@ -39,29 +39,26 @@ class Scratch {
                 pb.inheritIO();
                 Process process = pb.start();
                 process.waitFor();
-            } catch (InterruptedException e) {
+            } catch (InterruptedException e) {//try
                 e.printStackTrace();
-            } finally {
+            } finally {//finally
                 tempScript.delete();
-            }
-        }
+            }//finally
+        }//executeCommands
 
         public static File createTempScript() throws IOException {
             File tempScript = File.createTempFile("script", null);
 
-            Writer streamWriter = new OutputStreamWriter(new FileOutputStream(
-                    tempScript));
+            Writer streamWriter = new OutputStreamWriter(new FileOutputStream(tempScript));
             PrintWriter printWriter = new PrintWriter(streamWriter);
-
             printWriter.println("#!/bin/bash");
-            printWriter.println("source /home/58/n01382358/.bash_profile");
-	    printWriter.println("cd /home/58/n01382358/COP4620/Project1/Compilers_step1");
-           // printWriter.println("cat *.micro | grun Little prog -tokens > Output.txt");
-            printWriter.println("ls > Output.txt");
-
+	//printWriter.println("source /home/58/n01382358/.bash_profile");
+	    printWriter.println(System.getProperty("user.dir") + "/Output.txt");
+          //printWriter.println("grun Little prog System.getProperty("user.dir") + "/*micro" -tokens > Output.txt");
+		printWriter.println("ls > Output.txt");
             printWriter.close();
 
             return tempScript;
-        }
+        }//createTempScript
     }//end of Scratch
 
