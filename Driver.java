8f6df29fe54ec989574cfac5a119039d69980b57
Dominic Harshaw <harshawdominic@yahoo.com>
2021-02-01 16:50:35 -0500

    Final Patch for Step1 Little.g4 works and so does Driver.java

--- Driver.java
+++ Driver.java
@@ -16,75 +16,42 @@ public class Driver{
 		ANTLRInputStream input = new ANTLRInputStream(is);
 		LittleLexer lexer = new LittleLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		LittleParser parser = new LittleParser(tokens);
-		
-		ParseTree tree = parser.prog(); // parse; start at prog
-		ParseTreeWalker walker = new ParseTreeWalker();
-		System.out.println(tree.toStringTree(parser));
+		spaceWalk(tokens);
+	}
 
-	/*try{
-		FileWriter fileWriter = new FileWriter(System.getProperty("user.dir") + "/tmp.txt");
-		Process p = Runtime.getRuntime().exec("java -Xmx500M -cp //home/50/n00851750/COP4620/lib/antlr-4.9-complete.jar:$CLASSPATH org.antlr.v4.gui.TestRig' grun Little prog " + args + "-tokens ");
-		//BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String line;
-		//System.out.println(reader.readLine());
-		InputStream in = p.getInputStream();
-		//System.out.println(in.available());
-		for(int x=0;x<in.available();x++)/*while((line = reader.readLine()) != null){
-			//fileWriter.write(in.read()/*line + "\n");
-			System.out.println(in.read());
+	static void spaceWalk(CommonTokenStream tok){
+		int lim = tok.getNumberOfOnChannelTokens();
+
+		int tmp = 0;
+		for(int x=1;tmp!=-1;x++){
+			tmp = tok.LT(x).getType();
+			if(tmp>= 1 && tmp <= 7){
+				switch(tmp){
+					case 1:
+						System.out.println("TokenType: KEYWORD");
+						break;
+					case 2:
+						System.out.println("TokenType: INDENTIFIER");
+						break;
+					case 3:
+						System.out.println("TokenType: INTLITERAL");
+						break;
+					case 5:
+						System.out.println("TokenType: STRINGLITERAL");
+						break;
+					case 4:
+						System.out.println("TokenType: FLOATLITERAL");
+						break;
+					case 6:
+						System.out.println("TokenType: OPERATOR");
+						break;
+					default :
+						break;
+				}
+					System.out.println("Value: " + tok.LT(x).getText());
+			}
 		}
 
-		fileWriter.close();*/
-
-		//walker.walk(new LilListener(), tree);
-		
-		//System.out.println(tree.getSymbol());
-		//walker.walk(new tokenTyper(), tree);
-		//System.out.println(voc.getSymbolicName());
-		//
-		//walk(tree);
-	/*}catch (Exception e){
-		e.printStackTrace();
-	}*/
-
-	/*	System.out.println(tokens.size());
-	for(int x=0; x< tokens.size();x++){
-
-	try{
-		Token tmp = tokens.LT(x);
-		if(tmp.getTokenIndex()!=-1)
-			System.out.println(tmp.getText());
-	}catch (Exception e){
-		
-	}
-	}*/
-
-		walker.walk(new typeVal(), tree);
-
 	}
 
-
-	static void walk(ParseTree t){
-		//Object tmp = t.getPayload();
-		//TerminalNodeImpl tmp0 = t.
-		System.out.println("Token Type: " + /*tmp.getToken() +*/ "\nValue : " + t.getText());
-		
-		int lim = t.getChildCount();
-		for(int i=0;i<lim;i++)
-			walk(t.getChild(i));
-	}
-
-
-
-	/*static String walkA(ProgContext ctx){
-		return ctx.getChild(0).getText();
-	}*/
-
-
-	public static class typeVal extends LittleBaseListener {
-		public void enterProg(LittleParser.ProgContext ctx){
-			System.out.println("Token Type: " + ctx.getPaylod() + "\nValue: " + ctx.getChild(0).getText());
-		}
-	}
 }
